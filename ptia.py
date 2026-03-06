4d7d7d904b48f464f06e69e3bee66e7022da234c
0SP <official.intsp@gmail.com>
2026-03-06 02:35:32 +0200

    1126.0A1005.9800.7.9

--- ptia.py
+++ ptia.py
@@ -1,7 +1,7 @@
 #|==============================================================|#
 # Made by IntSPstudio
 # Thank you for using this plugin!
-# Version: 0.0.2.110403
+# Version: 0.0.3.110603
 # ID: 980007009
 #|==============================================================|#
 
@@ -14,6 +14,7 @@ def load_menu(filename):
         return json.load(f)
 #MENU LOOP
 def show_menu(menu, start_page="P01"):
+    output = ""
     current_page = start_page
     continuity = 1
     while continuity == 1:
@@ -25,7 +26,7 @@ def show_menu(menu, start_page="P01"):
         #TA
         page = menu.get(current_page, {})
         title = page.get("Title", "Unknown")
-        print(f"==] ======== {title} ========")
+        print(f"==] {title}")
         #TB
         rows = [(key, val) for key, val in page.items() if key.startswith("R")]
         rows.sort()
@@ -40,19 +41,27 @@ def show_menu(menu, start_page="P01"):
             if user_input < 1 or user_input > len(rows):
                 continue
             elif user_input == "":
-                continuity
+                continue
         except ValueError:
             continue
         #SELECTED ROW
         _, selected_item = rows[user_input - 1]
         next_page = selected_item.get("Link")
+        test = str(next_page)
+        #FUNCTION TEST
+        if test[0] == "F":
+            output = test
+            return output
+        #SELECT
         if next_page and next_page in menu:
             current_page = next_page
         elif next_page == "P00":
             continuity =0
+            output ="F000"
+            return output 
         else:
             current_page = start_page
 #START
 if __name__ == "__main__":
     menu = load_menu("example.json")
-    show_menu(menu, "P01")
+    result = show_menu(menu, "P01")
